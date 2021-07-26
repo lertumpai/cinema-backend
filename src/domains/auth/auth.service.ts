26c2259591071f66fd195e8d4e7987a0db9c221c
sorawit <SorawitL@arv.co.th>
2021-07-26 22:18:17 +0700

    implemented authen

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@nestjs/common'
+import { UnauthorizedException } from '@nestjs/common'
 import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
 
 import { AuthEntity } from '../../databases/postgres/entities/auth.entity'
 import { AuthRepository } from './auth.repository'
-import { DuplicationErrorException } from '../../errors/duplicationError.exception'
 import { AuthRegisterArgs, AuthArgs } from './auth.dto'
+
+import {
+  DuplicationErrorException,
+  NotFoundErrorException,
+} from '../../errors'
 
 @Injectable()
 export class AuthService {
+  private readonly secret = 'secret'
+
   constructor(
     private authRepository: AuthRepository
   ) {}
 
   async register(authRegisterArgs: AuthRegisterArgs): Promise<AuthEntity> {
     const { username, password, role } = authRegisterArgs
-    const createdUser = await this.authRepository.findOneByUsername(username)
+    const user = await this.authRepository.findOneByUsername(username)
 
-    if (createdUser) {
+    if (user) {
       throw new DuplicationErrorException('Username', username)
     }
 
@@ -28,8 +36,45 @@ export class AuthService {
     })
   }
 
-  async login(authArgs: AuthArgs): Promise<any> {
+  async validateUser(authArgs: AuthArgs): Promise<AuthEntity> {
     const { username, password } = authArgs
-    return null
+    const user = await this.authRepository.findOneByUsername(username)
+
+    if (!user) {
+      throw new NotFoundErrorException('Username', username)
+    }
+
+    if(!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Password invalid')
+    }
+
+    return user
+  }
+
+  getToken(user: AuthEntity): string {
+    const payload = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    }
+
+    return jwt.sign(payload, this.secret, { expiresIn: 3600 })
+  }
+
+  async login(authArgs: AuthArgs): Promise<string> {
+    const user = await this.validateUser(authArgs)
+    return this.getToken(user)
+  }
+
+  validateToken(token: string): any {
+    try {
+      if (!token || token.search('CINEMA')) {
+        return null
+      }
+
+      return jwt.verify(token.replace('CINEMA ', ''), this.secret)
+    } catch (e) {
+      return null
+    }
   }
 }
--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,11 @@
 import { HttpException } from '@nestjs/common'
 
+export class NotFoundErrorException extends HttpException {
+  constructor(name: string, value: string) {
+    super(`${name} ${value} is not found`, 400);
+  }
+}
+
 export class DuplicationErrorException extends HttpException {
   constructor(name: string, value: string) {
     super(`${name} ${value} is duplicated`, 400);
